be1561eb5e293ea7ba2e81e88c31f8fd3ae3c2ad
reach009 <sovanreach.c007@gmail.com>
2021-01-10 22:07:14 +0800

    update user context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,44 +9,6 @@ import routes from "./config/routes";
 import AppRoute from "./components/routes/AppRoute";
 
 function App() {
-  // const [userData, setUserData] = useState({
-  //   token: undefined,
-  //   user: undefined,
-  //   isAuthenticated: false,
-  // });
-
-  // useEffect(() => {
-  //   const checkedLogin = async () => {
-  //     const token = localStorage.getItem("auth-token");
-
-  //     if (token === null) {
-  //       localStorage.setItem("auth-token", "");
-  //       token = "";
-  //     }
-
-  //     const tokenResponse = await axios.post(
-  //       "http://localhost:5000/users/tokenIsValid",
-  //       null,
-  //       { headers: { "x-auth-token": token } }
-  //     );
-  //     if (tokenResponse.data) {
-  //       const userRes = await axios.get("http://localhost:5000/users/", {
-  //         headers: { "x-auth-token": token },
-  //       });
-
-  //       setUserData({
-  //         token: token,
-  //         user: userRes.data,
-  //         isAuthenticated: true,
-  //       });
-  //     }
-  //   };
-
-  //   checkedLogin();
-  // }, []);
-
-  // console.log("User data: " + JSON.stringify(userData.token));
-
   return (
     <UserProvider>
       <BrowserRouter>
--- frontend/src/context/userContext.js
+++ frontend/src/context/userContext.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useReducer, useEffect } from "react";
 
 let reducer = (userData, newUserData) => {
@@ -5,7 +6,6 @@ let reducer = (userData, newUserData) => {
     localStorage.removeItem("userData");
     return initialState;
   }
-
   return { ...userData, ...newUserData };
 };
 
@@ -24,6 +24,43 @@ function UserProvider(props) {
     reducer,
     localState || initialState
   );
+
+  useEffect(() => {
+    const checkedLogin = async () => {
+      const token = localStorage.getItem("auth-token");
+
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+
+      console.log(token);
+      try {
+        // check if the token is valid
+        const tokenResponse = await axios.post(
+          "http://localhost:5000/users/tokenIsValid",
+          null,
+          { headers: { "x-auth-token": token } }
+        );
+
+        // retrieve the user data
+        if (tokenResponse.data) {
+          const userRes = await axios.get("http://localhost:5000/users/", {
+            headers: { "x-auth-token": token },
+          });
+
+          localStorage.setItem("userData", JSON.stringify(userInfo));
+          // console.log("User info ", userInfo);
+        }
+      } catch (err) {
+        console.log(err.response.data);
+        localStorage.removeItem("userData");
+        // console.log("User info ", userInfo);
+      }
+    };
+
+    checkedLogin();
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("userData", JSON.stringify(userInfo));
